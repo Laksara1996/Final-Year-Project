a7c679a4579550dd4d3a4b1e1d4d869d5ef95a3c
Laksara <40088875+Laksara1996@users.noreply.github.com>
2020-10-20 00:47:56 +0530

    Neural Network Implementation

--- services/ac_control.py
+++ services/ac_control.py
@@ -1,42 +1,18 @@
+# Load libraries
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import pandas as pd
+import numpy as np
+
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.wrappers.scikit_learn import KerasClassifier
+
 import requests
-from flask import Flask, jsonify, make_response
-import json
+from flask import Flask, jsonify
 import os
 
 app = Flask(__name__)
-
-database_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-
-with open('{}/database/todo.json'.format(database_path), "r") as jsf:
-    todo_list = json.load(jsf)
-
-
-@app.route('/', methods=['GET'])
-def hello():
-    ''' Greet the user '''
-
-    return "Todo service is up"
-
-
-@app.route('/lists', methods=['GET'])
-def show_lists():
-    ''' Displays all the lists '''
-
-    tlists = []
-    for username in todo_list:
-        for lname in todo_list[username]:
-            tlists.append(lname)
-    return jsonify(lists=tlists)
-
-
-@app.route('/lists/<username>', methods=['GET'])
-def user_list(username):
-    ''' Returns a user oriented list '''
-
-    if username not in todo_list:
-        return "No list found"
-
-    return jsonify(todo_list[username])
 
 
 @app.route('/lists/air_condition', methods=['GET'])
@@ -44,7 +20,7 @@ def air_condition_lists():
     """ Get lists based on air_condition """
 
     number_array = get_air_condition_data()
-    return str(number_array[0])
+    return jsonify(number_array)
 
 
 @app.route('/lists/passenger_count', methods=['GET'])
@@ -52,7 +28,7 @@ def passenger_count_lists():
     """ Get lists based on passenger_count """
 
     number_array = get_passenger_count_data()
-    return str(number_array[0])
+    return jsonify(number_array)
 
 
 @app.route('/lists/window_opening', methods=['GET'])
@@ -60,12 +36,20 @@ def window_opening_lists():
     """ Get lists based on window_opening """
 
     number_array = get_window_opening_data()
-    return str(number_array[0])
+    return jsonify(number_array)
+
+
+@app.route('/lists/ac_status_output', methods=['GET'])
+def ac_status_output():
+    """ Get lists based on window_opening """
+
+    number_array = neural_network_function()
+    return jsonify(number_array)
 
 
 def get_air_condition_data():
     try:
-        req = requests.get("http://localhost:5000//data/air_condition_status")
+        req = requests.get("http://localhost:5000//data/airConditionStatus")
         req_text = req.text[1:-1]
         number = ""
         number_array = []
@@ -85,7 +69,7 @@ def get_air_condition_data():
 
 def get_passenger_count_data():
     try:
-        req = requests.get("http://localhost:5000//data/passenger_count")
+        req = requests.get("http://localhost:5000//data/passengerCount")
         req_text = req.text[1:-1]
         number = ""
         number_array = []
@@ -105,7 +89,7 @@ def get_passenger_count_data():
 
 def get_window_opening_data():
     try:
-        req = requests.get("http://localhost:5000//data/window_opening")
+        req = requests.get("http://localhost:5000//data/windowOpening")
         req_text = req.text[1:-1]
         number = ""
         number_array = []
@@ -121,6 +105,55 @@ def get_window_opening_data():
     except requests.exceptions.ConnectionError:
         return "Service unavailable"
     return number_array
+
+
+def neural_network_function():
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+    # fix random seed for reproducibility
+    seed = 7
+    np.random.seed(seed)
+
+    window_opening_data = [int(i) for i in get_window_opening_data()]
+    passenger_count_data = [int(i) for i in get_passenger_count_data()]
+
+    air_condition_data = [int(i) for i in get_air_condition_data()]
+
+    X = np.array((passenger_count_data, window_opening_data)).T
+    Y = air_condition_data
+
+    print("X")
+    print(X)
+    print("y")
+    print(Y)
+
+    def baseline_model():
+        # create model
+        model = Sequential()
+        model.add(Dense(32, input_dim=2, activation='relu'))
+        model.add(Dense(12, activation='relu'))
+        model.add(Dense(4, activation='softmax'))
+
+        # compile model
+        model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+        return model
+
+    estimator = KerasClassifier(build_fn=baseline_model, epochs=200, batch_size=5, verbose=0)
+
+    # split train and test
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.20, random_state=0)
+
+    estimator.fit(X_train, Y_train)
+
+    print(estimator.predict(X_test))
+    print('Accuracy: ', accuracy_score(Y_test, estimator.predict(X_test)))
+
+    df = pd.DataFrame(estimator.predict(X), columns=['output'])
+    print("output")
+    print(df['output'].values.tolist())
+
+    return df['output'].values.tolist()
 
 
 if __name__ == '__main__':
