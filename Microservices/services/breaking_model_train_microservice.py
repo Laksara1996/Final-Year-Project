288869acfed3f6b73cc67959f4be1a4fd81d2192
Sewwandi Nisansala <sewwanis@gmail.com>
2020-12-14 16:18:49 +0530

    breaking microservice added

---
+++ Microservices/services/breaking_model_train_microservice.py
@@ -0,0 +1,185 @@
+# Load libraries
+import numpy as np
+
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.wrappers.scikit_learn import KerasClassifier
+
+import requests
+from flask import Flask
+from flask_caching import Cache
+import os
+
+import json
+from json import JSONEncoder
+
+import time
+from threading import Timer
+
+
+class NumpyArrayEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return JSONEncoder.default(self, obj)
+
+
+class RepeatedTimer(object):
+    def __init__(self, interval, function, *args, **kwargs):
+        self._timer = None
+        self.interval = interval
+        self.function = function
+        self.args = args
+        self.kwargs = kwargs
+        self.is_running = False
+        self.start()
+
+    def _run(self):
+        self.is_running = False
+        self.start()
+        self.function(*self.args, **self.kwargs)
+
+    def start(self):
+        if not self.is_running:
+            self._timer = Timer(self.interval, self._run)
+            self._timer.start()
+            self.is_running = True
+
+    def stop(self):
+        self._timer.cancel()
+        self.is_running = False
+
+
+config = {
+    "DEBUG": True,  # some Flask specific configs
+    "CACHE_TYPE": "simple",  # Flask-Caching related configs
+    "CACHE_DEFAULT_TIMEOUT": 300
+}
+
+app = Flask(__name__)
+
+app.config.from_mapping(config)
+cache = Cache(app)
+
+# estimator = 0
+
+
+@app.route('/breaking/predict', methods=['GET'])
+@cache.cached(timeout=300)
+def predict_data():
+    start_time = time.time()
+    number_array = predict()
+    numpyData = {"array": number_array}
+    encodedNumpyData = json.dumps(numpyData, cls=NumpyArrayEncoder)  # use dump() to write array into file
+    print("---predict_data %s seconds ---" % (time.time() - start_time))
+    return encodedNumpyData
+
+
+@app.route('/breaking/output', methods=['GET'])
+@cache.cached(timeout=300)
+def output_data():
+    start_time = time.time()
+    number_array = output()
+    numpyData = {"array": number_array}
+    encodedNumpyData = json.dumps(numpyData, cls=NumpyArrayEncoder)  # use dump() to write array into file
+    print("---output_data %s seconds ---" % (time.time() - start_time))
+    return encodedNumpyData
+
+
+def get_x_train_data():
+    try:
+        req = requests.get("http://localhost:3001/breaking/x_train")
+        decodedArrays = json.loads(req.text)
+
+        finalNumpyArray = np.asarray(decodedArrays["array"])
+
+        print("x train length")
+        print(len(finalNumpyArray))
+
+    except requests.exceptions.ConnectionError:
+        return "Service unavailable"
+    return finalNumpyArray
+
+
+def get_y_train_data():
+    try:
+        req = requests.get("http://localhost:3001/breaking/y_train")
+        decodedArrays = json.loads(req.text)
+
+        finalNumpyArray = np.asarray(decodedArrays["array"])
+
+        print("y train length")
+        print(len(finalNumpyArray))
+
+    except requests.exceptions.ConnectionError:
+        return "Service unavailable"
+    return finalNumpyArray
+
+
+def get_x_test_data():
+    try:
+        req = requests.get("http://localhost:3001/breaking/x_test")
+        decodedArrays = json.loads(req.text)
+
+        finalNumpyArray = np.asarray(decodedArrays["array"])
+
+    except requests.exceptions.ConnectionError:
+        return "Service unavailable"
+    return finalNumpyArray
+
+
+def get_input_data():
+    try:
+        req = requests.get("http://localhost:3001/breaking/input")
+        decodedArrays = json.loads(req.text)
+
+        finalNumpyArray = np.asarray(decodedArrays["array"])
+
+    except requests.exceptions.ConnectionError:
+        return "Service unavailable"
+    return finalNumpyArray
+
+
+def estimator_train():
+    # os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+    # seed = 7
+    # np.random.seed(seed)
+
+    global estimator
+
+    def baseline_model():
+        # create model
+        model = Sequential()
+        model.add(Dense(500, activation='relu', input_dim=3))
+        model.add(Dense(100, activation='relu'))
+        model.add(Dense(50, activation='relu'))
+        model.add(Dense(3, activation='softmax'))
+
+        # compile model
+        # optimizer='adam' learning rate
+        model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+        return model
+
+    estimator = KerasClassifier(build_fn=baseline_model, epochs=200, batch_size=20, verbose=0)
+    estimator.fit(get_x_train_data(), get_y_train_data())
+    return estimator
+
+
+def predict():
+    # global estimator
+    estimator = estimator_train()
+    predict_value = estimator.predict(get_x_test_data())
+    return predict_value
+
+
+def output():
+    # global estimator
+    estimator = estimator_train()
+    output_value = estimator.predict(get_input_data())
+    return output_value
+
+
+# rt = RepeatedTimer(50, estimator_train)  # it auto-starts, no need of rt.start()
+
+if __name__ == '__main__':
+    app.run(port=3103, debug=True)
