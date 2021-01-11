4cfbfd9169c6a7bdf10b912cf3a8b23ac1fb1dbd
Laksara <40088875+Laksara1996@users.noreply.github.com>
2021-01-11 20:51:39 +0530

    Start up page + sign in page + sign up page

---
+++ fyp/lib/Initial_Pages/sign_up.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+
+class SignUp extends StatefulWidget {
+  @override
+  _SignUpState createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Padding(
+            padding: EdgeInsets.all(10),
+            child: ListView(
+              children: <Widget>[
+                Container(
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      'FYP',
+                      style: TextStyle(
+                          color: Colors.blue,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 30),
+                    )),
+                Container(
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      'Sign Up',
+                      style: TextStyle(fontSize: 20),
+                    )),
+                Container(
+                  padding: EdgeInsets.all(10),
+                  child: TextField(
+                    controller: nameController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'User Name',
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(10),
+                  child: TextField(
+                    controller: emailController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Email',
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+                  child: TextField(
+                    obscureText: true,
+                    controller: passwordController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Password',
+                    ),
+                  ),
+                ),
+                SizedBox(height: 30),
+                Container(
+                    height: 50,
+                    padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                    child: RaisedButton(
+                      textColor: Colors.white,
+                      color: Colors.blue,
+                      child: Text('Sign Up'),
+                      onPressed: () {
+                        print(nameController.text);
+                        print(emailController.text);
+                        print(passwordController.text);
+                      },
+                    )),
+                // Container(
+                //     child: Row(
+                //       children: <Widget>[
+                //         Text('Does not have account?'),
+                //         FlatButton(
+                //           textColor: Colors.blue,
+                //           child: Text(
+                //             'Sign Up',
+                //             style: TextStyle(fontSize: 20),
+                //           ),
+                //           onPressed: () {
+                //             //signup screen
+                //           },
+                //         )
+                //       ],
+                //       mainAxisAlignment: MainAxisAlignment.center,
+                //     )
+                // )
+              ],
+            )));
+  }
+}
--- fyp/lib/main.dart
+++ fyp/lib/main.dart
@@ -1,6 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:fyp/Initial_Pages/sign_in.dart';
+import 'package:fyp/Initial_Pages/sign_up.dart';
 
-void main() => runApp(MaterialApp(home: Home()));
+import 'Initial_Pages/start_up_page.dart';
+
+void main() => runApp(MaterialApp(
+      initialRoute: '/start',
+      routes: {
+        '/start': (context) => Start_Up_Page(),
+        '/test': (context) => Home(),
+        '/signin': (context) => SignIn(),
+        '/signup': (context) => SignUp(),
+
+
+      },
+    ));
 
 class Home extends StatelessWidget {
   @override
