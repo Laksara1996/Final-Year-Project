b9b71c978c00b2677f3197d8d0a6317109df1e70
Sewwandi Nisansala <sewwanis@gmail.com>
2021-02-04 21:54:51 +0530

    raspberry delegation import error fixed

---
+++ Microservices/Roof Microservices/Without Libraries/services/accuracy_microservice_rasp2.py
@@ -0,0 +1,158 @@
+# Load libraries
+from sklearn.metrics import accuracy_score
+import numpy as np
+
+import requests
+from flask import Flask
+
+import json
+from json import JSONEncoder
+
+import time
+from threading import Timer
+import datetime
+
+a = datetime.datetime.now()
+
+
+class NumpyArrayEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return JSONEncoder.default(self, obj)
+
+
+class RepeatedTimer(object):
+    def __init__(self, interval, function, *args, **kwargs):
+        self._timer = None
+        self.interval = interval
+        self.function = function
+        self.args = args
+        self.kwargs = kwargs
+        self.is_running = False
+        self.start()
+
+    def _run(self):
+        self.is_running = False
+        self.start()
+        self.function(*self.args, **self.kwargs)
+
+    def start(self):
+        if not self.is_running:
+            self._timer = Timer(self.interval, self._run)
+            self._timer.start()
+            self.is_running = True
+
+    def stop(self):
+        self._timer.cancel()
+        self.is_running = False
+
+
+app = Flask(__name__)
+
+ac_accuracy = 0
+speed_accuracy = 0
+
+
+@app.route('/ac_control/accuracy', methods=['GET'])
+def ac_status_accuracy():
+    global ac_accuracy
+
+    start_time = time.time()
+    accuracy_value = ac_accuracy
+    print("---ac accuracy %s seconds ---" % (time.time() - start_time))
+    return str(accuracy_value)
+
+
+@app.route('/speed/accuracy', methods=['GET'])
+def speed_accuracy_output():
+    global speed_accuracy
+    start_time = time.time()
+    accuracy_value = speed_accuracy
+    print("---speed accuracy %s seconds ---" % (time.time() - start_time))
+    return str(accuracy_value)
+
+
+def get_ac_control_y_test_data():
+    try:
+        req = requests.get("http://192.168.1.102:3001/ac_control/y_test")
+        decodedArrays = json.loads(req.text)
+
+        finalNumpyArray = np.asarray(decodedArrays["array"])
+
+    except requests.exceptions.ConnectionError:
+        return "Service unavailable"
+    return finalNumpyArray
+
+
+def get_ac_control_predict_data():
+    try:
+        req = requests.get("http://192.168.1.108:3003/ac_control/predict")
+        decodedArrays = json.loads(req.text)
+
+        finalNumpyArray = np.asarray(decodedArrays["array"])
+
+    except requests.exceptions.ConnectionError:
+        return "Service unavailable"
+    return finalNumpyArray
+
+
+def get_speed_y_test_data():
+    try:
+        req = requests.get("http://192.168.1.102:3001/speed/y_test")
+        decodedArrays = json.loads(req.text)
+
+        finalNumpyArray = np.asarray(decodedArrays["array"])
+
+    except requests.exceptions.ConnectionError:
+        return "Service unavailable"
+    return finalNumpyArray
+
+
+def get_speed_predict_data():
+    try:
+        req = requests.get("http://192.168.1.108:3201/speed/predict")
+        decodedArrays = json.loads(req.text)
+
+        finalNumpyArray = np.asarray(decodedArrays["array"])
+
+    except requests.exceptions.ConnectionError:
+        return "Service unavailable"
+    return finalNumpyArray
+
+
+def ac_control_accuracy():
+    global ac_accuracy
+
+    y_test = get_ac_control_y_test_data()
+    predict_data = get_ac_control_predict_data()
+
+    print("y_test len", len(y_test))
+    print("predict len", len(predict_data))
+
+    ac_accuracy = accuracy_score(y_test[:len(predict_data)], predict_data)
+    print('AC Control Accuracy: ', ac_accuracy)
+
+
+def speed_accuracy_check():
+    global speed_accuracy
+    y_test = get_speed_y_test_data()
+    predict_data = get_speed_predict_data()
+
+    print("y_test len", len(y_test))
+    print("predict len", len(predict_data))
+
+    speed_accuracy = accuracy_score(y_test[:len(predict_data)], predict_data)
+    print('Speed Accuracy: ', speed_accuracy)
+
+
+ac_accuracy_automated = RepeatedTimer(25, ac_control_accuracy)
+speed_accuracy_automated = RepeatedTimer(25, speed_accuracy_check)
+
+
+b = datetime.datetime.now()
+print("Execution Time:")
+print(b-a)
+
+if __name__ == '__main__':
+    app.run(port=3002, host='0.0.0.0', debug=True)
---
+++ Microservices/Roof Microservices/Without Libraries/services/speed_model_train_rasp1.py
@@ -0,0 +1,424 @@
+# Load libraries
+import numpy as np
+from numpy import argmax
+
+import requests
+from flask import Flask
+from flask_caching import Cache
+import os
+
+import json
+from json import JSONEncoder
+
+import time
+from threading import Timer
+import datetime
+
+a = datetime.datetime.now()
+
+
+class NumpyArrayEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return JSONEncoder.default(self, obj)
+
+
+class RepeatedTimer(object):
+    def __init__(self, interval, function, *args, **kwargs):
+        self._timer = None
+        self.interval = interval
+        self.function = function
+        self.args = args
+        self.kwargs = kwargs
+        self.is_running = False
+        self.start()
+
+    def _run(self):
+        self.is_running = False
+        self.start()
+        self.function(*self.args, **self.kwargs)
+
+    def start(self):
+        if not self.is_running:
+            self._timer = Timer(self.interval, self._run)
+            self._timer.start()
+            self.is_running = True
+
+    def stop(self):
+        self._timer.cancel()
+        self.is_running = False
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+def sigmoid_der(x):
+    return sigmoid(x) * (1 - sigmoid(x))
+
+
+def softmax(A):
+    expA = np.exp(A)
+    return expA / expA.sum(axis=1, keepdims=True)
+
+
+def predict(wh, bh, wo, bo, X_test):
+    zh = np.dot(X_test, wh) + bh
+    ah = sigmoid(zh)
+    zo = np.dot(ah, wo) + bo
+    ao = softmax(zo)
+    return ao
+
+
+# config = {
+#     "DEBUG": True,  # some Flask specific configs
+#     "CACHE_TYPE": "simple",  # Flask-Caching related configs
+#     "CACHE_DEFAULT_TIMEOUT": 300
+# }
+
+app = Flask(__name__)
+
+# app.config.from_mapping(config)
+# cache = Cache(app)
+
+y_predict_array = []
+
+
+@app.route('/speed/predict', methods=['GET'])
+# @cache.cached(timeout=300)
+def predict_data():
+    start_time = time.time()
+    number_array = predict_output()
+    numpyData = {"array": number_array}
+    encodedNumpyData = json.dumps(numpyData, cls=NumpyArrayEncoder)  # use dump() to write array into file
+    print("---speed predict_data %s seconds ---" % (time.time() - start_time))
+    print("----speed predict amount of data = %s ------" % len(number_array))
+    return encodedNumpyData
+
+
+@app.route('/speed/output', methods=['GET'])
+# @cache.cached(timeout=300)
+def output_data():
+    start_time = time.time()
+    number_array = output()
+    numpyData = {"array": number_array}
+    encodedNumpyData = json.dumps(numpyData, cls=NumpyArrayEncoder)  # use dump() to write array into file
+    print("---speed output_data %s seconds ---" % (time.time() - start_time))
+    print("----speed output amount of data = %s ------" % len(number_array))
+    return encodedNumpyData
+
+
+def get_x_train_data():
+    try:
+        req = requests.get("http://192.168.1.102:3001/speed/x_train")
+        decodedArrays = json.loads(req.text)
+
+        finalNumpyArray = np.asarray(decodedArrays["array"])
+
+    except requests.exceptions.ConnectionError:
+        return "Service unavailable"
+    return finalNumpyArray
+
+
+def get_y_train_data():
+    try:
+        req = requests.get("http://192.168.1.102:3001/speed/y_train")
+        decodedArrays = json.loads(req.text)
+
+        finalNumpyArray = np.asarray(decodedArrays["array"])
+
+    except requests.exceptions.ConnectionError:
+        return "Service unavailable"
+    return finalNumpyArray
+
+
+def get_x_test_data():
+    try:
+        req = requests.get("http://192.168.1.102:3001/speed/x_test")
+        decodedArrays = json.loads(req.text)
+
+        finalNumpyArray = np.asarray(decodedArrays["array"])
+
+    except requests.exceptions.ConnectionError:
+        return "Service unavailable"
+    return finalNumpyArray
+
+
+def get_y_test_data():
+    try:
+        req = requests.get("http://192.168.1.102:3001/speed/y_test")
+        decodedArrays = json.loads(req.text)
+
+        finalNumpyArray = np.asarray(decodedArrays["array"])
+
+    except requests.exceptions.ConnectionError:
+        return "Service unavailable"
+    return finalNumpyArray
+
+
+def get_input_data():
+    try:
+        req = requests.get("http://192.168.1.102:3001/speed/input")
+        decodedArrays = json.loads(req.text)
+
+        finalNumpyArray = np.asarray(decodedArrays["array"])
+
+    except requests.exceptions.ConnectionError:
+        return "Service unavailable"
+    return finalNumpyArray
+
+
+def get_fog_wh():
+    try:
+        req = requests.get("http://192.168.1.112:4201/fog/wh")
+        decodedArrays = json.loads(req.text)
+
+        finalNumpyArray = np.asarray(decodedArrays["array"])
+
+    except requests.exceptions.ConnectionError:
+        return "Service unavailable"
+    return finalNumpyArray
+
+
+def get_fog_bh():
+    try:
+        req = requests.get("http://192.168.1.112:4201/fog/bh")
+        decodedArrays = json.loads(req.text)
+
+        finalNumpyArray = np.asarray(decodedArrays["array"])
+
+    except requests.exceptions.ConnectionError:
+        return "Service unavailable"
+    return finalNumpyArray
+
+
+def get_fog_wo():
+    try:
+        req = requests.get("http://192.168.1.112:4201/fog/wo")
+        decodedArrays = json.loads(req.text)
+
+        finalNumpyArray = np.asarray(decodedArrays["array"])
+
+    except requests.exceptions.ConnectionError:
+        return "Service unavailable"
+    return finalNumpyArray
+
+
+def get_fog_bo():
+    try:
+        req = requests.get("http://192.168.1.112:4201/fog/bo")
+        decodedArrays = json.loads(req.text)
+
+        finalNumpyArray = np.asarray(decodedArrays["array"])
+
+    except requests.exceptions.ConnectionError:
+        return "Service unavailable"
+    return finalNumpyArray
+
+
+def get_cloud_wh():
+    try:
+        req = requests.get("http://34.126.124.227:5201/cloud/wh")
+        decodedArrays = json.loads(req.text)
+
+        finalNumpyArray = np.asarray(decodedArrays["array"])
+
+    except requests.exceptions.ConnectionError:
+        return "Service unavailable"
+    return finalNumpyArray
+
+
+def get_cloud_bh():
+    try:
+        req = requests.get("http://34.126.124.227:5201/cloud/bh")
+        decodedArrays = json.loads(req.text)
+
+        finalNumpyArray = np.asarray(decodedArrays["array"])
+
+    except requests.exceptions.ConnectionError:
+        return "Service unavailable"
+    return finalNumpyArray
+
+
+def get_cloud_wo():
+    try:
+        req = requests.get("http://34.126.124.227:5201/cloud/wo")
+        decodedArrays = json.loads(req.text)
+
+        finalNumpyArray = np.asarray(decodedArrays["array"])
+
+    except requests.exceptions.ConnectionError:
+        return "Service unavailable"
+    return finalNumpyArray
+
+
+def get_cloud_bo():
+    try:
+        req = requests.get("http://34.126.124.227:5201/cloud/bo")
+        decodedArrays = json.loads(req.text)
+
+        finalNumpyArray = np.asarray(decodedArrays["array"])
+
+    except requests.exceptions.ConnectionError:
+        return "Service unavailable"
+    return finalNumpyArray
+
+
+def get_roof_accuracy():
+    try:
+        req = requests.get("http://192.168.1.107:3002/speed/accuracy")
+        accuracy = float(req.text)
+
+    except requests.exceptions.ConnectionError:
+        return "Service unavailable"
+    return accuracy
+
+
+def get_fog_accuracy():
+    try:
+        req = requests.get("http://192.168.1.112:4002/speed/accuracy")
+        accuracy = float(req.text)
+
+    except requests.exceptions.ConnectionError:
+        return "Service unavailable"
+    return accuracy
+
+
+def get_cloud_accuracy():
+    try:
+        req = requests.get("http://34.126.124.227:5002/speed/accuracy")
+        accuracy = float(req.text)
+
+    except requests.exceptions.ConnectionError:
+        return "Service unavailable"
+    return accuracy
+
+
+def model_train():
+    global y_predict_array
+
+    y_train = get_y_train_data()
+    # print("y_train", y_train)
+    x_train = get_x_train_data()
+    x_test = get_x_test_data()
+    y_test = get_y_test_data()
+
+    if len(x_test) == len(y_test) and len(x_train) == len(y_train):
+
+        # create a matrix for one hot encoding
+        one_hot_labels = np.zeros((len(y_train), 6))
+        for i in range(len(y_train)):
+            # print("Y_train", y_train)
+            # print("Y_train i", y_train[i])
+            one_hot_labels[i, y_train[i]] = 1
+
+        input_nodes = 5
+        hidden_nodes = 8
+        output_labels = 6
+        wh = np.random.rand(input_nodes, hidden_nodes)
+        bh = np.random.randn(hidden_nodes)
+        wo = np.random.rand(hidden_nodes, output_labels)
+        bo = np.random.randn(output_labels)
+        lr = 10e-4
+
+        error_cost = []
+
+        for epoch in range(5000):
+            ############# feedforward
+
+            # Phase 1
+            zh = np.dot(x_train, wh) + bh
+            ah = sigmoid(zh)
+            zo = np.dot(ah, wo) + bo
+            ao = softmax(zo)
+
+            ########## Back Propagation
+
+            ########## Phase 1
+
+            dcost_dzo = ao - one_hot_labels
+            dzo_dwo = ah
+
+            dcost_wo = np.dot(dzo_dwo.T, dcost_dzo)
+
+            dcost_bo = dcost_dzo
+
+            ########## Phases 2
+
+            dzo_dah = wo
+            dcost_dah = np.dot(dcost_dzo, dzo_dah.T)
+            dah_dzh = sigmoid_der(zh)
+            dzh_dwh = x_train
+            dcost_wh = np.dot(dzh_dwh.T, dah_dzh * dcost_dah)
+
+            dcost_bh = dcost_dah * dah_dzh
+
+            # Update Weights ================
+
+            wh -= lr * dcost_wh
+            bh -= lr * dcost_bh.sum(axis=0)
+
+            wo -= lr * dcost_wo
+            bo -= lr * dcost_bo.sum(axis=0)
+
+            if epoch % 200 == 0:
+                loss = np.sum(-one_hot_labels * np.log(ao))
+                # print('Loss function value: ', loss)
+                error_cost.append(loss)
+
+        # End of for loop (End of training phase)
+
+        roof_accuracy = get_roof_accuracy()
+        fog_accuracy = get_fog_accuracy()
+        cloud_accuracy = get_cloud_accuracy()
+
+        print("roof", roof_accuracy)
+        print("fog", fog_accuracy)
+
+        if fog_accuracy > roof_accuracy:
+            wh = get_fog_wh()
+            bh = get_fog_bh()
+            wo = get_fog_wo()
+            bo = get_fog_bo()
+
+            if cloud_accuracy > fog_accuracy:
+                wh = get_cloud_wh()
+                bh = get_cloud_bh()
+                wo = get_cloud_wo()
+                bo = get_cloud_bo()
+
+        # Make predictions
+        predictions = predict(wh, bh, wo, bo, x_test)
+
+        y_predict = []
+        for i in range(len(y_test)):
+            n = argmax(predictions[i])
+            y_predict.append(n)
+        y_predict_array = np.array(y_predict)
+
+        print("y_pred", y_predict_array)
+
+
+def predict_output():
+    global y_predict_array
+    # model_train()
+
+    return y_predict_array
+
+
+def output():
+    global y_predict_array
+    # y_predict = model_train()
+
+    return y_predict_array
+
+
+model_train_automated = RepeatedTimer(15, model_train)
+
+b = datetime.datetime.now()
+print("Execution Time:")
+print(b-a)
+
+if __name__ == '__main__':
+    app.run(port=3201, host='0.0.0.0')
